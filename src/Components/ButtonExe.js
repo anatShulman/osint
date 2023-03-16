a3fca59701c31ca0718c83a96468454f55cae010
anatShulman <96178499+anatShulman@users.noreply.github.com>
2023-03-17 01:11:33 +0200

    Delete src directory

--- src/Components/ButtonExe.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-import Button from '@mui/material/Button';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
-export default function ButtonExe() {
-  return (
-
-    <Button startIcon={<FileDownloadIcon />} variant="contained" style={{ right: "20%", top: "9%", position: "absolute" }}>Download File</Button>
-  )
-}
