a3e178ed03295685fad3bd8075472927f73df8ee
Matan Kichler <matankic@gmail.com>
2023-04-19 23:05:08 +0300

    m

---
+++ BE/get_urls.js
@@ -0,0 +1,10 @@
+const checkUrls = require('./urlscan_list.js');
+
+const URLS = [
+  'http://lists-prizes.fun/?u%5C=40dwkwf&o%5C=8vkp4zm&t%5C=arc4',
+  'https://datehub.life/?u=y2ykaew&o=2xzp89r&m=1&t=1504&utm_campaign=d4',
+  'https://romanceroundup.life/?u=54lkaeg&o=grmpkza&m=1&t=sexxys&cid=sexxys',
+  'http://landing.globify.in/wp-content/sidelightex.php?utm_campaign=rv'
+];
+
+checkUrls(URLS);
--- BE/urlscan_list.js
+++ BE/urlscan_list.js
@@ -3,13 +3,6 @@ const headers = {
   'Content-Type': 'application/json'
 };
 
-const urls = [
-  'http://lists-prizes.fun/?u%5C=40dwkwf&o%5C=8vkp4zm&t%5C=arc4',
-  'https://datehub.life/?u=y2ykaew&o=2xzp89r&m=1&t=1504&utm_campaign=d4',
-  'https://romanceroundup.life/?u=54lkaeg&o=grmpkza&m=1&t=sexxys&cid=sexxys',
-  'http://landing.globify.in/wp-content/sidelightex.php?utm_campaign=rv'
-];
-
 const scanUrl = async (url) => {
   try {
     const data = {
@@ -39,7 +32,7 @@ const scanUrl = async (url) => {
   }
 };
 
-const checkUrls = async () => {
+const checkUrls = async (urls) => {
   const maliciousUrls = [];
   const promises = urls.map(async (url) => {
     const result = await scanUrl(url);
@@ -51,4 +44,4 @@ const checkUrls = async () => {
   console.log(maliciousUrls);
 };
 
-checkUrls();
+module.exports = checkUrls;
